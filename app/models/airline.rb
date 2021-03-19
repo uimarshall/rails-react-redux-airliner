50a86a1f2ca06032f652ec29fded5c81975f71e9
uimarshall <uimarshall@gmail.com>
2021-03-19 15:51:18 +0100

    Add class methods to the airline model

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,2 +1,18 @@
 class Airline < ApplicationRecord
+    has_many :reviews
+    # When we create a new Airline, we take the name of the airline and slugify it,
+    # we then set it as the slug value b4 adding it to the Db
+    before_create :slugify
+    def slugify
+        self.slug = name.parameterize #create url save version of the name
+        
+    end
+ # Get the average score for an Airline review
+    def avg_score
+        reviews.average(:score).round(2).to_f
+        
+    end
+
+   
+
 end
