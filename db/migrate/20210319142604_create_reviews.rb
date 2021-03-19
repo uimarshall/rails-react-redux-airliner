b23299aac0a4c798b604a3db30d7c48c4018cb2d
uimarshall <uimarshall@gmail.com>
2021-03-19 15:28:55 +0100

    Create models for review

--- db/migrate/20210319142604_create_reviews.rb
+++ db/migrate/20210319142604_create_reviews.rb
@@ -4,7 +4,7 @@ class CreateReviews < ActiveRecord::Migration[6.0]
       t.string :title
       t.string :description
       t.integer :score
-      t.belong_to :airline
+      t.belongs_to :airline, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_19_141137) do
+ActiveRecord::Schema.define(version: 2021_03_19_142604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,4 +23,15 @@ ActiveRecord::Schema.define(version: 2021_03_19_141137) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.integer "score"
+    t.bigint "airline_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["airline_id"], name: "index_reviews_on_airline_id"
+  end
+
+  add_foreign_key "reviews", "airlines"
 end
