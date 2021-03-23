038356fa83b81f97693593163d20ef76267606f2
uimarshall <uimarshall@gmail.com>
2021-03-23 11:06:56 +0100

    configure routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#index'
+  namespace :api do
+    namespace :v1 do
+      resources :airlines, param: :slug #slug will be used as default id instead of id in Db
+      resources :reviews, only: [:create, :destroy]
+    end  
+  end
+  # If a non-existed route is requested for,route back to index page
+  get '*path', to: 'pages#index', via: :all
 end
