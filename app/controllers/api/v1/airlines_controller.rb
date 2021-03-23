b4ec7f21bdadfaf4603163d1aff4cb4d0b8d9f55
uimarshall <uimarshall@gmail.com>
2021-03-23 11:40:29 +0100

    Add controllers

---
+++ app/controllers/api/v1/airlines_controller.rb
@@ -0,0 +1,61 @@
+module Api
+  module V1
+    class AirlinesController < ApplicationController
+      def index
+        # Get all the airlines from our db and render data using d serializer
+        airlines = Airline.all
+        render json: AirlineSerializer.new(airlines, options).serialized_json
+      end
+
+      # Show single airline
+      def show
+        airline = Airline.find_by(slug: params[:slug])
+        render json: AirlineSerializer.new(airline, options).serialized_json
+      end
+
+      def create
+        airline = Airline.new(airline_params)
+        if airline.save
+          render json: AirlineSerializer.new(airline).serialized_json
+
+        else
+          render json: { error: airline.errors.messages }, status: 422
+
+        end
+      end
+
+      def update
+        airline = Airline.find_by(slug: params[:slug])
+        if airline.update(airline_params)
+          render json: AirlineSerializer.new(airline, options).serialized_json
+
+        else
+          render json: { error: airline.errors.messages }, status: 422
+
+        end
+      end
+
+      def destroy
+        airline = Airline.find_by(slug: params[:slug])
+        if airline.destroy
+          head :no_content
+
+        else
+          render json: { error: airline.errors.messages }, status: 422
+
+        end
+      end
+
+      private
+
+      def airline_params
+        params.require(:airline).permit(:name, :image_url)
+      end
+
+      # Handling N + 1 call to the Db, we include optional resources(reviews)
+      def options
+        @options ||= { incude: %i[reviews] }
+      end
+    end
+  end
+end
